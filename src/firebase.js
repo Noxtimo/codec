825f64491fe12d87c1dc489f832edee65241213f
Noxtimo <canbuulo1231@gmail.com>
2024-11-14 16:46:40 +0000

    added the env

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,15 @@ import { getFirestore } from "firebase/firestore";
 import { GoogleAuthProvider } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
 
+// Use environment variables stored in .env
 const firebaseConfig = {
-  apiKey: "AIzaSyAJIC-NxA6OxFPlQo1yLP-T3hN4rgZAR9I",
-  authDomain: "codec-academy.firebaseapp.com",
-  projectId: "codec-academy",
-  storageBucket: "codec-academy.firebasestorage.app",
-  messagingSenderId: "880270944361",
-  appId: "1:880270944361:web:ddf4bff4ae5e76cda19af8",
-  measurementId: "G-90ECSJKZVV",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
